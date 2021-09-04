3ef1b2ed3b2a7a1a565cdca08ca14510e0392119
alvianzf <alvianzf@gmail.com>
2021-09-04 11:34:58 +0700

    push code

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.example.weatherapp
 
+import android.os.AsyncTask
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import org.json.JSONObject
+import org.w3c.dom.Text
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+        val city: String = "singapore"
+        val api: String = "1fe0f2f277e8b35c0712458f06ef1ffc"
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            weatherTask().execute()
+        }
+
+        inner class weatherTask() : AsyncTask<String, Void, String>() {
+            override fun onPreExecute() {
+                super.onPreExecute()
+
+                findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
+                findViewById<TextView>(R.id.errorText).visibility = View.GONE
+            }
+
+            override fun doInBackground(vararg params: String?): String? {
+                var response:String?
+
+                try {
+                    response = URL("https://api.openweathermap.org/data/2" +
+                            ".5/weather?q=$city&unit=metrics&appid=$api").readText(Charsets.UTF_8)
+                } catch (e: Exception) {
+                    response = null;
+                }
+
+                return response
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                try {
+                    val jsonObj = JSONObject(result)
+                    val main = jsonObj.getJSONObject("main")
+                    val sys = jsonObj.getJSONObject("sys")
+                    val wind = jsonObj.getJSONObject("wind")
+                    val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+//                    Populate data
+                    findViewById<TextView>(R.id.wind).text = wind.getJSONObject("speed").toString()
+                    print(jsonObj)
+
+                    findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                    findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
+                } catch (e: Exception) {
+                    findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                    findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+
+                }
+            }
+        }
 }
